be1fff1ffcd72df9c710e195757a6dc5b8063a56
Ngo Ngoc Sang <ngocsang26889@gmail.com>
2026-01-10 17:37:20 +0200

    10.8 codes

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,7 +1,8 @@
-import { Link } from "react-router-native";
-import { View, StyleSheet } from "react-native";
+import { Link, useNavigate } from "react-router-native";
+import { View, StyleSheet, TextInput } from "react-native";
 import theme from "../theme";
 import Text from "./Text";
+import { useFormik } from "formik";
 
 const styles = StyleSheet.create({
   container: {
@@ -22,17 +23,49 @@ const styles = StyleSheet.create({
     fontSize: theme.fontSizes.subheading,
     fontWeight: theme.fontWeights.bold,
   },
+  input: {
+    width: "100%",
+    padding: 12,
+    marginBottom: 16,
+    fontSize: theme.fontSizes.body,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: theme.borderRadius,
+  },
 });
 
 const SignIn = () => {
+  const navigate = useNavigate();
+  const initialValues = {
+    username: "",
+    password: "",
+  };
+  const onSubmit = (values) => {
+    console.log(values);
+    navigate("/");
+  };
+  const formik = useFormik({
+    initialValues,
+    onSubmit,
+  });
+
   return (
     <View style={styles.container}>
-      <Text fontSize="subheading" fontWeight="bold">
-        Sign-in view
-      </Text>
-
-      <Link to="/" style={styles.navButton}>
-        <Text style={styles.navButtonText}>Back to Repositories</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Username"
+        value={formik.values.username}
+        onChangeText={formik.handleChange("username")}
+      />
+      <TextInput
+        secureTextEntry={true}
+        style={styles.input}
+        placeholder="Password"
+        value={formik.values.password}
+        onChangeText={formik.handleChange("password")}
+      />
+      <Link to="/" style={styles.navButton} onPress={formik.handleSubmit}>
+        <Text style={styles.navButtonText}>Sign in</Text>
       </Link>
     </View>
   );
